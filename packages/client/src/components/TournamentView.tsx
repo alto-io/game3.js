e6195e584feceeaf721fcb67c2ef253e3673bb7e
yuri-alto <yuri@alto.io>
2020-08-13 14:21:47 +0800

    Change tournament view to class.

--- packages/client/src/components/TournamentView.tsx
+++ packages/client/src/components/TournamentView.tsx
@@ -5,68 +5,97 @@ import { Box, Flex, Text, Link } from "rimble-ui";
 
 import GameCard from '../components/GameCard';
 
-function TournamentView({ drizzle, drizzleState, drizzleStatus, account, networkId }) {
-  const [currentNetwork, setCurrentNetwork] = useState(null);
-  const [address, setAddress] = useState(null);
+const TEMP_TOURNEY = [{
+  name: "Tourney 1",
+  image: "RER",
+  button: "TEST"
+}]
 
-  const TEMP_TOURNEY = [{
-      name: "Tourney 1",
-      image: "RER",
-      button: "TEST"
-  }]
+// Optional parameters to pass into RimbleWeb3
+const RIMBLE_CONFIG = {
+  // accountBalanceMinimum: 0.001,
+  // requiredNetwork: 5777, // ganache
+  requiredNetwork: 4 // rinkeby
+};
 
-    // Optional parameters to pass into RimbleWeb3
-    const RIMBLE_CONFIG = {
-    // accountBalanceMinimum: 0.001,
-    requiredNetwork: 5777 // ganache
-    };
+class TournamentView extends React.Component<any, any> {
 
-  // Set account
-  useEffect(() => {
-    if (account) {
-      setAddress(account);
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentNetwork: null,
+      address: null
     }
-  }, [account]);
+  }
 
-  // Set current network
-  useEffect(() => {
+  componentDidMount() {
+    const { account, networkId, drizzleStatus, drizzle } = this.props
+
+    this.updateAddress(account)
+    this.updateDrizzle(networkId, drizzleStatus, drizzle)
+  }
+
+  componentWillReceiveProps(newProps) {
+    const { account, networkId, drizzleStatus, drizzle } = this.props
+    const { account: newAccount, networkId: newNetworkId, 
+      drizzleStatus: newDrizzleStatus, drizzle: newDrizzle } = newProps
+
+    if (account !== newAccount) {
+      this.updateAddress(account)
+    }
+    if (networkId !== newNetworkId || drizzleStatus !== newDrizzleStatus
+      || drizzle !== newDrizzle) {
+      this.updateDrizzle(newNetworkId, newDrizzleStatus, newDrizzle)
+    }
+  }
+
+  updateAddress = (address) => {
+    this.setState({ address })
+  }
+
+  updateDrizzle = (networkId, drizzleStatus, drizzle) => {
     if (networkId) {
-      setCurrentNetwork(networkId);
+      this.setState({ currentNetwork: networkId} );
     }
     if (!drizzleStatus.initialized && window.web3 && drizzle !== null) {
       window.web3.version.getNetwork((error, networkId) => {
-        setCurrentNetwork(parseInt(networkId));
+        this.setState({ currentNetwork: parseInt(networkId) } );
       });
     }
-  }, [networkId, drizzleStatus, drizzle]);
+  }
 
-  return (
-    <Box>
-      {
-        !drizzleState && (
-        <Box m={4}>
-          <ConnectionBanner
-            currentNetwork={currentNetwork}
-            requiredNetwork={RIMBLE_CONFIG.requiredNetwork}
-            onWeb3Fallback={null}
-          />
-        </Box>
-        )
-      }
-        <Box maxWidth={"1180px"} p={3} mx={"auto"}>
-            <Text my={4} />
-            <Flex justifyContent={"space-between"} mx={-3} flexWrap={"wrap"}>
-                {TEMP_TOURNEY.map(game => {
-                return (
-                    <GameCard
-                    game={game}
-                    />
-                );
-                })}
-            </Flex>
-        </Box>  
-    </Box>
-  );
+  render() {
+    const { drizzleState } = this.props
+    const { currentNetwork } = this.state
+    return (
+      <Box>
+        {
+          !drizzleState && (
+          <Box m={4}>
+            <ConnectionBanner
+              currentNetwork={currentNetwork}
+              requiredNetwork={RIMBLE_CONFIG.requiredNetwork}
+              onWeb3Fallback={null}
+            />
+          </Box>
+          )
+        }
+          <Box maxWidth={"1180px"} p={3} mx={"auto"}>
+              <Text my={4} />
+              <Flex justifyContent={"space-between"} mx={-3} flexWrap={"wrap"}>
+                  {TEMP_TOURNEY.map(game => {
+                  return (
+                      <GameCard
+                      game={game}
+                      />
+                  );
+                  })}
+              </Flex>
+          </Box>  
+      </Box>
+    );
+  }
 }
 
 /*
