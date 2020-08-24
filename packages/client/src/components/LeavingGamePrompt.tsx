cafdec9d0374cb0207746b2fd366b2e001ed89fb
joshmatthew <joshmatthewtalplacido@gmail.com>
2020-08-24 14:25:56 +0800

    Fixed the bug when pressing the browser's back button

--- packages/client/src/components/LeavingGamePrompt.tsx
+++ packages/client/src/components/LeavingGamePrompt.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { RouteComponentProps } from '@reach/router';
+import disableBrowserBackButton from 'disable-browser-back-navigation';
 
 interface IProps extends RouteComponentProps {
 	when?: boolean; // This is like the "when" prop of react-router's Prompt component
@@ -22,9 +23,9 @@ export default class LeavingGamePrompt extends Component<IProps, IState> {
 
 	componentDidMount() {
     console.log("Mounted");
-		window.history.pushState(null, document.title, window.location.href);
+	  window.history.pushState(null, null, null);
 
-	  // window.addEventListener('popstate', this.eventDispatcher);
+	  window.addEventListener('popstate', this.eventDispatcher);
 		window.addEventListener("beforeunload", this.eventDispatcher);
 		window.addEventListener("onpathchange", this.eventDispatcher);
 	}
@@ -33,12 +34,13 @@ export default class LeavingGamePrompt extends Component<IProps, IState> {
     console.log("Dismounted");
 		window.removeEventListener("onpathchange", this.eventDispatcher);
 		window.removeEventListener("beforeunload", this.eventDispatcher);
-		// window.removeEventListener('popstate', this.eventDispatcher);
+		window.removeEventListener('popstate', this.eventDispatcher);
 	}
 
 
 	handleEvents(e: any) {
 		const { alertMessage } = this.state;
+   
 		switch(e.type) {
   		  	case "beforeunload": 
   		  		console.log("Test beforeunload");
@@ -50,9 +52,10 @@ export default class LeavingGamePrompt extends Component<IProps, IState> {
   		  		console.log("Test popstate");
     
             if(window.confirm(alertMessage)) {
+              window.removeEventListener('popstate', this.eventDispatcher);
               window.history.back();
             } else {
-              window.history.pushState(null, document.title, window.location.href);  
+              window.history.pushState(null, document.title, window.location.href);
             }
   		  		break;
   		  	case "onpathchange":
