8fbbc5ccf3d3552d5cf2ace8aa1261c3c1b44f2d
ealmario <ee.almario@gmail.com>
2020-08-24 14:50:32 +0800

    Add content for mobile navigation

--- packages/client/src/components/NavBarHamburger.tsx
+++ packages/client/src/components/NavBarHamburger.tsx
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
+import { Box, Flex } from "rimble-ui";
+import styled from "styled-components";
+import { isThisHour } from "date-fns";
 
+const BurgerContainer = styled(Flex)`
+  cursor: pointer;
+`
+
+const BurgerDiv = styled(Box)`
+  background-color: #212121;
+  border-radius:  25px;
+  margin: 2px auto;
+  width: 25px;
+  height: 3px;
+`
 class NavBarHamburger extends Component {
   render() {
     return (
       <>
-        <div></div>
-        <div></div>
-        <div></div>
-
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+      <BurgerContainer 
+          justifyContent={"center"} 
+          alignItems={"center"} 
+          flexDirection={"column"}
+          onClick={this.props.handleBurger} 
+      >
+        <BurgerDiv></BurgerDiv>
+        <BurgerDiv></BurgerDiv>
+        <BurgerDiv></BurgerDiv>
+      </BurgerContainer>
       </>
     )
   }
--- packages/client/src/components/OutplayLoginHeaderMobile.tsx
+++ packages/client/src/components/OutplayLoginHeaderMobile.tsx
@@ -1,19 +1,77 @@
 import React, { Component } from "react";
-import { Flex, Button, Link, Image } from "rimble-ui";
+import { Flex, Button, Link, Image, Box, Text } from "rimble-ui";
 import styled  from "styled-components";
+
+import NavBarHamburger from "./NavBarHamburger";
 
 const StyledButton = styled(Button)`
   font-family: 'Apercu Light';
-  font-size: 0.7rem;
-  letter-spacing: 0.5px;
+  font-size: 0.75rem;
+  letter-spacing: 0.4px;
   text-transform: uppercase;
 `
+
+const StyledTextLink = styled(Link)`
+  &:hover {
+    text-decoration: none;
+  }
+`
+const Dropdown = styled.ul`
+  background-color: #fff;
+  display: flex;
+  color: #4E174F;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-end;
+  list-style: none;
+  font-size: 1rem;
+  letter-spacing: 0.5px;
+  margin: 0;
+
+  li {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 0.75rem;
+    width: 100%;
+  }
+
+  img {
+    width: 1.25rem;
+    height: 1.25rem;
+    margin-right: 0.5rem;
+  }
+
+  .title {
+    color: #2b2c36;
+    font-weight: 600;
+  }
+`
+
 class OutplayLoginHeaderMobile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    }
+    this.handleBurger = this.handleBurger.bind(this);
+  }
+
+  handleBurger(e) {
+    this.setState({isOpen: !this.state.isOpen});
+  }
+
   render () {
     const {
-      handleClickLogo,
+      account, 
+      accountBalance,
+      accountValidated,
+      handleClickLogo, 
       handleConnectAccount,
-      logo
+      shortenAddress, 
+      logo, 
+      balanceIcon,
+      walletIcon
     } = this.props;
 
     return (
@@ -28,16 +86,51 @@ class OutplayLoginHeaderMobile extends Component {
                 onClick={handleClickLogo}
             />
           </Link>
-      
-          <StyledButton 
-              color="primary"
-              size="small"
-              onClick={handleConnectAccount}>
-            Connect your Wallet
-          </StyledButton>
 
-           {/* Navbar Hamburger Here  */}
+          <Flex>
+            {account && accountValidated ? "" : (
+            <StyledButton 
+                color="primary"
+                size="small"
+                mr={3}
+                onClick={handleConnectAccount}>
+             Connect your Wallet
+            </StyledButton>
+            )}
+            
+
+            <NavBarHamburger handleBurger={this.handleBurger}/>
+          </Flex> 
         </Flex>
+
+        <Dropdown>
+          {account && accountValidated ? (
+            <>
+              {/* ID */}
+              <li>
+                <img src={walletIcon} alt="wallet-icon"/>
+                <p><span className="title">Connected as </span>{shortenAddress(account)}</p>
+              </li>
+
+              {/* Balance */}
+              <li>
+                <img src={balanceIcon} mr={2} alt="balance-icon"/>
+                <p><span className="title">Balance </span>{accountBalance.toString()} ETH</p>
+              </li>
+            </>
+          ) : ""}
+          <li>
+            <StyledTextLink 
+                href="https://outplay.games" 
+                target="_blank"
+                title={"To outplay.games"}
+                fontSize={"1rem"}
+                mr={3}
+            >
+              About Us
+            </StyledTextLink>
+          </li>
+        </Dropdown>
       </>
     )
   }
