545538980cac1df05250312a47430e956ebad942
yuri-alto <yuri@alto.io>
2020-09-02 15:02:26 +0800

    Empty payout events view.

---
+++ packages/client/src/components/PayoutEventsView.tsx
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import { drizzleConnect } from "@drizzle/react-plugin";
+import { Flex, Flash, Card, Box, Text } from "rimble-ui";
+
+import styled from "styled-components";
+
+const StyledFlex = styled(Flex)`
+  flex-direction: column;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`
+const StyledCard = styled(Card)`
+  padding: 1rem 0.5rem;
+  margin: 1rem;
+  
+  @media screen and (min-width: 640px) {
+    padding: 2rem 1rem;
+    margin: 0 1rem;
+  }
+`
+
+class PayoutEventsView extends Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentNetwork: null,
+      address: null,
+    }
+  }
+
+  componentDidMount() {
+    const { address, networkId, drizzleStatus, drizzle} =  this.props
+
+    this.updateAddress(address)
+    this.updateDrizzle(networkId, drizzleStatus, drizzle)
+  }
+
+  componentWillReceiveProps(newProps) {
+    const { address, networkId, drizzleStatus, drizzle } = this.props
+    const { address: newAddress, networkId: newNetworkId, 
+      drizzleStatus: newDrizzleStatus, drizzle: newDrizzle } = newProps
+
+    if (address !== newAddress) {
+      this.updateAddress(newAddress)
+    }
+    if (networkId !== newNetworkId || drizzleStatus !== newDrizzleStatus
+      || drizzle !== newDrizzle) {
+      this.updateDrizzle(newNetworkId, newDrizzleStatus, newDrizzle)
+    }
+  }
+
+  updateAddress = (address) => {
+    this.setState({ address })
+  }
+
+  updateDrizzle = (networkId, drizzleStatus, drizzle) => {
+    if (networkId) {
+      this.setState({ currentNetwork: networkId} );
+    }
+    if (!drizzleStatus.initialized && window.web3 && drizzle !== null) {
+      window.web3.version.getNetwork((error, networkId) => {
+        this.setState({ currentNetwork: parseInt(networkId) } );
+      });
+    }
+    if (drizzleStatus.initialized && window.web3 && drizzle !== null) {
+      this.fetchEvents();
+    }
+  }
+
+  fetchEvents = async () => {
+  }
+
+    render() {
+      const { account, accountValidated, drizzle, setRoute } = this.props;
+
+      return (
+        <StyledCard px={3} py={4}>
+          {account && accountValidated ? (
+            <>
+              <Text>PayoutEventsView</Text>
+            </>
+          ) : (
+            <Flash> You have to be logged in to view. </Flash>
+          )}
+        </StyledCard>  
+      );
+      }
+
+}
+/*
+ * Export connected component.
+ */
+const mapStateToProps = state => {
+  return {
+    drizzleStatus: state.drizzleStatus,
+    address: state.accounts[0],
+    networkId: state.web3.networkId
+  };
+};
+  
+export default drizzleConnect(PayoutEventsView, mapStateToProps);
