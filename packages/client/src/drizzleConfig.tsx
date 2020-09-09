8a976ab9e19a0f863ba8d04d0bc537c7d319685c
yuri-alto <yuri@alto.io>
2020-09-09 12:28:22 +0800

    Specify required network in the public env file.

--- packages/client/src/drizzleConfig.tsx
+++ packages/client/src/drizzleConfig.tsx
@@ -1,7 +1,5 @@
 const drizzleConfig = {
-    requiredNetwork: 5777, // Network Id that contract is deployed to 1=mainnet, 4=rinkeby, etc
-    //requiredNetwork: 4,
-  };
-  
-  export default drizzleConfig;
-  
+  requiredNetwork: parseInt(process.env.REACT_APP_NETWORK_ID)
+};
+
+export default drizzleConfig;
