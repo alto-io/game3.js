d1a2c2394378ae7e6731b6a20c3669a3797b2dbe
Paul Gadi <paulgadi@gmail.com>
2020-12-27 14:12:56 +0800

    display SDK button

--- packages/op-arcade-sdk/src/Op.svelte
+++ packages/op-arcade-sdk/src/Op.svelte
@@ -10,19 +10,7 @@ import logo from './assets/game3js.png'
 
 
 <main>
-  <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-    <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src={logo} alt="Woman's Face">
-    <div class="text-center space-y-2 sm:text-left">
-      <div class="space-y-0.5">
-        <p class="text-lg text-black font-semibold">
-          Erin Lindford
-        </p>
-        <p class="text-gray-500 font-medium">
-          Product Engineer
-        </p>
-      </div>
-      <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
-    </div>
-  </div>
-  
+  <button class="m-2 fixed bottom-0 left-0 inline-flex items-center justify-center w-12 h-12 mr-2 transition-colors duration-150 bg-indigo-700 rounded-full focus:shadow-outline hover:bg-indigo-800">
+    <img class="w-10 h-10 fill-current" alt="g3js logo" src={logo}>
+  </button>
 </main>
