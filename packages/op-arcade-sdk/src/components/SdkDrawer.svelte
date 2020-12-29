90f225a0b84284b02bea6cc1c1af7073def224f6
Paul Gadi <paulgadi@gmail.com>
2020-12-29 23:25:06 +0800

    add sdk.connect to drawer

--- packages/op-arcade-sdk/src/components/SdkDrawer.svelte
+++ packages/op-arcade-sdk/src/components/SdkDrawer.svelte
@@ -1,6 +1,6 @@
 <script>
     import { url, apiKey, opSdk, SDK_STATES } from '../stores.js'
-    import { fade, fly, slide } from 'svelte/transition'
+    import { fade, fly } from 'svelte/transition'
     
 import logo from '../assets/game3js.png'
 
@@ -19,7 +19,7 @@ on:click={() => visible = !visible}>
     <div transition:fade={{duration: 100}} class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50" on:click={() => visible = !visible}/>
 
     <aside
-    transition:fly={{duration:400, x:-50}}
+    in:fade out:fly={{duration:400, x:-100}}
     class="transform top-0 left-0 w-64 bg-white fixed h-full overflow-auto ease-in-out duration-300 z-30"
     >
     <span          
@@ -120,42 +120,55 @@ on:click={() => visible = !visible}>
       </span>
       <span>Contact</span></span
     >
+
     <div class="fixed bottom-0 w-full">
-      <button
-        class="flex items-center p-4 text-white bg-blue-500 hover:bg-blue-600 w-full"
+
+        <span
+        class="flex items-center p-4 hover:bg-indigo-500 hover:text-white "
+        ><span class="mr-2">
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            class="w-6 h-6"
+          >
+            <path
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+        </span>
+        <span>{$url}</span></span
       >
-        <svg
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          class="h-6 w-6 mr-2"
-        >
-          <path
-            d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
-          ></path>
-        </svg>
-        <span>Share</span>
-      </button>
-      <a
-        rel="noopener"
-        class="flex items-center p-4 bg-orange-700 text-white "
-        href="https://www.buymeacoffee.com/fayazahmed"
-        target="_blank"
-      >
-        <img
-          class="mr-2 h-8 w-auto"
-          src="/sidebar/bmc.svg"
-          alt="Buy Me Coffee"
-        />
-        <p>
-          <span class="font-bold">Buy me a Coffee</span>
-          <br />
-          <span class="text-sm text-white">Help me keep this site alive</span>
-        </p>
-      </a>
+        
+
+      <div class="flex items-center p-4 bg-blue-500 w-full">
+        {#if $opSdk.state == SDK_STATES.NOT_CONNECTED}    
+            <input class="mr-2 py-2 px-1 bg-white text-gray-700 placeholder-gray-500 shadow-md rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+            placeholder="API key"
+            bind:value={$apiKey}>
+            <button class="bg-purple-600 text-white text-base font-semibold py-2 px-2 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+                on:click={opSdk.connect}>
+                <span >
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      ></path>
+                    </svg>
+                  </span>
+            </button>
+        {/if}
+      </div>
     </div>
   </aside>
 
