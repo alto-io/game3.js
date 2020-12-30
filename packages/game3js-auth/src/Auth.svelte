b86da3bbd81be90bafbc3c72a93836db14790e41
Paul Gadi <paulgadi@gmail.com>
2020-12-30 20:49:23 +0800

    attach game3js-auth as svelte component | set login and sdk states

---
+++ packages/game3js-auth/src/Auth.svelte
@@ -0,0 +1,36 @@
+<script>
+import { onMount } from "svelte";
+
+
+  export const SERVER_TYPES = {
+    NAKAMA: "nakama"
+  }
+
+  export const SDK_STATES = {
+    READY: "ready",
+    INITIALIZED: "initialized"
+  }
+
+  export const LOGIN_STATES = {
+    LOGGED_IN: "logged in",
+    LOGGED_OUT: "logged out"
+  }
+
+  export let sdkState = SDK_STATES.READY;
+
+  export let options;
+
+  export function getSdkState() {
+    return sdkState
+  }
+
+  export function connect() {
+    return LOGIN_STATES.LOGGED_IN;
+  }
+
+  onMount(() => {
+    console.log(options);
+    sdkState = SDK_STATES.INITIALIZED;
+  });
+  
+</script>
--- packages/game3js-auth/src/main.js
+++ packages/game3js-auth/src/main.js
@@ -1,25 +1,21 @@
-export const SERVER_TYPE = {
-	NAKAMA: "nakama"
-}
-
-export const SDK_STATE = {
-	READY: "ready",
-	INITIALIZED: "initialized"
-}
+import Auth from './Auth.svelte';
 
 const DEFAULT_CONFIG = {
-	type: SERVER_TYPE.NAKAMA,
+	type: "nakama",
 	url: "http://127.0.0.1",
 	port: 7350,
 	key: "defaultkey"
 }
 
+
 const G3JSAuth = (options) => {
 
-	return {
-		state: SDK_STATE.INITIALIZED,
-		config: DEFAULT_CONFIG
-	}
+	return new Auth({
+		target: document.body,
+		props: {
+			options: DEFAULT_CONFIG
+		}
+	});
 }
 
 // attach to window
