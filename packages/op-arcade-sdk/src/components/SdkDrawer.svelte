b3508be6620ebe28d3a241db579b29d6b9d6866b
Paul Gadi <paulgadi@gmail.com>
2021-01-06 16:38:00 +0800

    replace apiKey with tourney-id | add connect to frontend

--- packages/op-arcade-sdk/src/components/SdkDrawer.svelte
+++ packages/op-arcade-sdk/src/components/SdkDrawer.svelte
@@ -156,7 +156,7 @@ on:click={() => visible = !visible}>
       <div class="flex items-center p-4 bg-blue-500 w-full">
         {#if $opSdk.state == SDK_STATES.NOT_CONNECTED}    
             <input class="mr-2 py-2 px-1 bg-white text-gray-700 placeholder-gray-500 shadow-md rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-            placeholder="API key"
+            placeholder="tourney-id (xxxx-xxxx-..)"
             bind:value={$apiKey}>
             <button class="bg-purple-600 text-white text-base font-semibold py-2 px-2 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
                 on:click={opSdk.connect}>
